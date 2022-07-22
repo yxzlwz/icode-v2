283f67ff58e50d2388a835989537d74b208861ea
YuzhenQin <1@jin-dan.site>
2022-07-22 08:04:00 +0800

    [UPDATE] python-minifyer

--- back/app.py
+++ back/app.py
@@ -5,7 +5,11 @@ import time
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from hunxiao import hunxiao
+import python_minifier
+
+def hunxiao(code: str) -> str:
+    return python_minifier.minify(code)
+
 
 app = Flask(__name__)
 CORS(app)
